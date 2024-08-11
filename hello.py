c2097e7a818ba4a33e5f6c77956deccadd0dff55
kimkanovsky <kimkanovsky@yandex.ru>
2024-08-11 18:50:08 +0300

    proj-9

--- hello.py
+++ hello.py
@@ -1,6 +1,9 @@
-"""
-This module performs a simple division and merges two dictionaries.
-"""
+'''Module performs simple division and merges two dictionaries.'''
+
+import logging
+
+# Настройка базового конфигурации логирования
+logging.basicConfig(level=logging.INFO)
 
 asd = 1
 bsd = 2
@@ -8,8 +11,10 @@ csd = asd / bsd
 dsd = {'hello': 'world'} | {'foo': 'bar'}
 
 def main() -> None:
-    """Main function to run the module."""
-    print(csd)
+    """Run the main logic of the module."""
+    
+    # Используем логирование вместо print
+    logging.info('Result of division: %s', csd)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
